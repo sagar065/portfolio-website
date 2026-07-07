11b29df95caa7f9e92c3669ef69003a3572281fd
Sagar singh <sagarbhandari243@gmail.com>
2026-07-07 20:27:37 +0530

    Update ProjectCard component to change demo link from "#" to "#Home" for improved navigation

--- src/components/projectSection/ProjectCard.js
+++ src/components/projectSection/ProjectCard.js
@@ -46,7 +46,7 @@ const ProjectCard = ({project}) => {
                         </a>
                     ) : 
                     (
-                        <a href = "#" className={`flex-1 text-center px-4 py-2 rounded-lg font-medium transition duration-300
+                        <a href = "#Home" className={`flex-1 text-center px-4 py-2 rounded-lg font-medium transition duration-300
                         target="_blank" rel="noopener noreferrer"
                             ${theme === "light" ? " text-richblack-900 bg-richblue-200 border border-purple hover:bg-richblue-400 " : " text-pure-greys-25 bg-richblack-500 hover:bg-richblack-700"}`}>
                             View Demo
